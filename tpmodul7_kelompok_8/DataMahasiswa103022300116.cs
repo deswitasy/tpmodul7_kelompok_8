3a3e149cdbe8de253621ed5260833fc8bfe3c626
syaaisya <aisyahsa948@gmail.com>
2025-04-07 18:29:31 +0800

    menambahkan class DataMahasiswa103022300116

---
+++ tpmodul7_kelompok_8/DataMahasiswa103022300116.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.IO;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace tpmodul7_kelompok_8
+{
+    public class Nama
+    {
+        public string depan { get; set; }
+        public string belakang { get; set; }
+
+    }
+
+    public class DataMahasiswa
+    {
+        public Nama nama { get; set; }
+        public string nim { get; set; }
+        public string fakultas { get; set; }
+    }
+    internal class DataMahasiswa103022300116
+    {
+        public static void ReadJSON()
+        {
+            string filePath = "tp7_1_103022300116.json";
+            if (File.Exists(filePath))
+            {
+                try
+                {
+                    string jsonString = File.ReadAllText(filePath);
+                    DataMahasiswa mhs = JsonSerializer.Deserialize<DataMahasiswa>(jsonString);
+
+                    if (mhs != null && mhs.nama != null)
+                    {
+                        Console.WriteLine($"Nama {mhs.nama.depan} {mhs.nama.belakang} dengan nim {mhs.nim}, dari fakultas {mhs.fakultas}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Data JSON tidak lengkap atau tidak sesuai format.");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Terjadi kesalahan saat memproses file JSON:");
+                    Console.WriteLine(e.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("File JSON tidak ditemukan!");
+            }
+        }
+
+    }
+}
--- tpmodul7_kelompok_8/Program.cs
+++ tpmodul7_kelompok_8/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.IO;
+using System.Text.Json;
+using tpmodul7_kelompok_8;
+
+class Program
+{
+    public static void Main()
+    {
+
+        DataMahasiswa103022300116.ReadJSON();
+
+    }
+}
