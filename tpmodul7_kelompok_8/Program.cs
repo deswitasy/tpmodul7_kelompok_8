4ac934f140efeeb6d894d269a02ab9664be3b108
apprliaaaaaa <liania470@gmail.com>
2025-04-07 22:52:38 +0700

    menambahkan class kuliahmahasiswa_103022300052

--- tpmodul7_kelompok_8/Program.cs
+++ tpmodul7_kelompok_8/Program.cs
@@ -2,15 +2,22 @@
 using System;
 using System.IO;
 using System.Text.Json;
+using Newtonsoft.Json;
 using tpmodul7_kelompok_8;
 
 class Program
 {
-    public static void Main()
+    
+    static void Main(string[] args)
     {
-
         DataMahasiswa103022300116.ReadJSON();
         var mahasiswa = new datamahasiswa_103022300052_apriliani();
         mahasiswa.ReadJSON();
+        kuliahmahasiswa_103022300052 read = new kuliahmahasiswa_103022300052();
+        read.ReadJSONList();
+       
+        Console.ReadKey();
+      
     }
+
 }
---
+++ tpmodul7_kelompok_8/kuliahmahasiswa_103022300052.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace tpmodul7_kelompok_8
+{
+    public class matkul_103022300052
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+    }
+
+    public class kuliahmahasiswa_103022300052
+    {
+        public List<matkul_103022300052> courses { get; set; }
+
+        public void ReadJSONList()
+        {
+            try
+            {
+                // Baca file JSON
+                string json = File.ReadAllText("tp7_2_103022300052.json");
+
+                // Deserialize JSON ke object
+                kuliahmahasiswa_103022300052 data = JsonConvert.DeserializeObject<kuliahmahasiswa_103022300052>(json);
+
+                // Tampilkan hasil deserialisasi
+                Console.WriteLine("Daftar Mata Kuliah:");
+                foreach (var m in data.courses)
+                {
+                    Console.WriteLine($"Kode: {m.code}, Nama: {m.name}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Terjadi error saat membaca JSON: {ex.Message}");
+            }
+        }
+    }
+}
