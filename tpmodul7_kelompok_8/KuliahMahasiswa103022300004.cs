ef5c2d18e9cf4fd5ca4d6a5695bd6482da212a41
deswitasy <deswitas06@gmail.com>
2025-04-07 21:42:16 +0700

    Menambahkan class KuliahMahasiswa103022300004

---
+++ tpmodul7_kelompok_8/KuliahMahasiswa103022300004.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace tpmodul7_kelompok_8
+{
+    public class Course
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+    }
+
+    public class KuliahMahasiswa103022300004
+    {
+        public List<Course> courses { get; set; }
+
+        public static void ReadJSON()
+        {
+            string filePath = "tp7_2_103022300004.json";
+            string json = File.ReadAllText(filePath);
+            KuliahMahasiswa103022300004 data = JsonSerializer.Deserialize<KuliahMahasiswa103022300004>(json);
+
+            Console.WriteLine("Daftar mata kuliah yang diambil:");
+            int i = 1;
+            foreach (var course in data.courses)
+            {
+                Console.WriteLine($"MK {i} {course.code} - {course.name}");
+                i++;
+            }
+        }
+    }
+
+}
--- tpmodul7_kelompok_8/Program.cs
+++ tpmodul7_kelompok_8/Program.cs
@@ -1,10 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using tpmodul7_kelompok_8;
+
 class Program
 {
     static void Main(string[] args)
     {
         DataMahasiswa103022300004 obj = new DataMahasiswa103022300004();
         obj.ReadJSON();
+        KuliahMahasiswa103022300004.ReadJSON();
+
     }
+
 }
 
