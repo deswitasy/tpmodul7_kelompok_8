455f4a21b72dbb795424bb1cf5bc9a1756e8eaf3
deswitasy <deswitas06@gmail.com>
2025-04-07 21:27:13 +0700

    menambahkan class DataMahasiswa103022300004

--- tpmodul7_kelompok_8/Program.cs
+++ tpmodul7_kelompok_8/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataMahasiswa103022300004 obj = new DataMahasiswa103022300004();
+        obj.ReadJSON();
+    }
+}
+
